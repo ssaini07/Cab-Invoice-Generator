337336971d97c970da096aeb8b52829f052f3e1e
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-05 00:07:57 +0530

    Added : Performed operation to calculate total fare

--- src/main/java/com/bridgelabz/cabinvoicegenerator/service/InvoiceService.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/service/InvoiceService.java
@@ -1,7 +1,14 @@
 package com.bridgelabz.cabinvoicegenerator.service;
 
 public class InvoiceService {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice generator!!!");
+
+    //Declaring instance variables.
+    private final double COST_PER_KM = 10.0;
+    private final int COST_PER_MIN = 1;
+    private final double MIN_FARE = 5.0;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_FARE);
     }
 }
--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -1,4 +1,29 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import com.bridgelabz.cabinvoicegenerator.service.InvoiceService;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_whenCalculateFare_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateFare(2.0, 5);
+        Assert.assertEquals(25, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeTestCase2_whenCalculateFare_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateFare(1.0, 5);
+        Assert.assertEquals(15, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_whenCalculateFareIsLessThan5_ShouldReturnTotalFareAs5() {
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateFare(0.1, 1);
+        Assert.assertEquals(5, totalFare, 0.0);
+    }
 }
