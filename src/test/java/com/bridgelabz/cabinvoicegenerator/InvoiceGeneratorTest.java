38c7fe35c2b4a7839d3f21fa84ac9658e86ff8c7
Shubham saini <78027352+ssaini07@users.noreply.github.com>
2022-01-05 13:08:19 +0530

    Update InvoiceGeneratorTest.java

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -19,11 +19,4 @@ public class InvoiceGeneratorTest {
         double totalFare = invoiceService.calculateFare(1.0, 5);
         Assert.assertEquals(15, totalFare, 0.0);
     }
-
-    @Test
-    public void givenDistanceAndTime_whenCalculateFareIsLessThan5_ShouldReturnTotalFareAs5() {
-        InvoiceService invoiceService = new InvoiceService();
-        double totalFare = invoiceService.calculateFare(0.1, 1);
-        Assert.assertEquals(5, totalFare, 0.0);
-    }
 }
