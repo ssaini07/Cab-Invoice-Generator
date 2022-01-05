42cd4c826958a3e72f9523d6d1b043d7cf7b33f8
Shubham saini <78027352+ssaini07@users.noreply.github.com>
2022-01-05 13:06:32 +0530

    Update InvoiceGeneratorTest.java

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -29,14 +29,4 @@ public class InvoiceGeneratorTest {
         Assert.assertEquals(5, totalFare, 0.0);
     }
 
-    @Test
-    public void givenMultipleRides_whenCalculateFare_ShouldReturnAggregateTotalFare() {
-        InvoiceService invoiceService = new InvoiceService();
-        Ride[] rides = {new Ride(2.0, 5),
-                new Ride(1.0, 5),
-                new Ride(0.1, 1)};
-        InvoiceSummary expectedSummary = new InvoiceSummary(3, 45);
-        InvoiceSummary summary = invoiceService.calculateFare(rides);
-        Assert.assertEquals(expectedSummary, summary);
-    }
 }
